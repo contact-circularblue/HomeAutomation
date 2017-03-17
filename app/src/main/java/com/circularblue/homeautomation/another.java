7e8687199fbbc287e00f199335d2167dfd299c66
PoojaK <khunger.pooja86@gmail.com>
2017-03-17 17:48:27 +0530

    Commit 1.0

---
+++ app/src/main/java/com/circularblue/homeautomation/another.java
@@ -0,0 +1,13 @@
+package com.circularblue.homeautomation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class another extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
